33afe2217189ad94c9b89036fb2c064257196d9a
Arrow Sparrow <dev@aroceu.cloud>
2024-11-29 20:50:22 -0500

    Update a.php

--- trainer/a.php
+++ trainer/a.php
@@ -336,8 +336,9 @@ padding: 10px;
 }
 
 strike{
-	color: #B0DDE8;
-	font: normal 100% 'Inconsolata';
+	color: #fff;
+  font: normal 100% 'Inconsolata';
+  background: #B0DDE8;
 }
 
 blockquote{
@@ -427,6 +428,10 @@ ul{
 	text-align: center;
 	width: auto;
 }
+
+li img{
+	margin-top: 0!important;
+}
     
 }
 
@@ -481,8 +486,8 @@ ul{
 <li>Statistically, my favorite type is dragon, though I chalk this up to dragons being my favorite animal in general. Steel types are second.</li>
 <li>I had an addiction to buying Pok&eacute;mon plushies for a short period in 2016 so now I own a lot of Pok&eacute;mon plushies. And I love every single one of them. Maybe I'll post a pic one day.</li>
 <li>Even though it was totally overpriced, I own a few things from the Pok&eacute;mon Stitch collection.</li>
-<li><img src="tcg.jpg" style="float: right; height: 80px; padding-left: 10px;padding-top: 0px;">My first Pok&eacute;mon card was the only bit of Pok&eacute;mon I ever really had between 1998 and 2004 (well, aside from the VHS tapes of <i>Pok&eacute;mon: the First Movie</i> and the first 3 episodes) &mdash; Jigglypuff from the 1999 Jungle expansion.</li>
-<li><img src="jigglypuff.jpg" style="float: left; height: 80; padding-right: 10px;padding-top: 10px;"> I don't have that card anymore, but I did recently buy a new one &mdash; Kingdra from the Shrouded Fable expansion.</li>
+<li><img src="jigglypuff.jpg" style="float: right; height: 90px; padding-left: 10px; padding-top: 0px;margin-top: -45px;">My first Pok&eacute;mon card was the only bit of Pok&eacute;mon I ever really had between 1998 and 2004 (well, aside from the VHS tapes of <i>Pok&eacute;mon: the First Movie</i> and the first 3 episodes) &mdash; Jigglypuff from the 1999 Jungle expansion.</li>
+<li><img src="tcg.jpg" style="float: right; height: 90; padding-right: 10px;padding-top: 10px;margin-top: -20px;"> I don't have that card anymore, but I did recently buy a new one &mdash; Kingdra from the Shrouded Fable expansion.</li>
 </ul>
 </div>
 <div style="clear: both;"></div>
