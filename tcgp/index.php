e548796fce6938ec1d2e9879c3aa7fa7fa87cfd4
Arrow Sparrow <dev@aroceu.cloud>
2025-02-06 23:54:20 -0500

    Update index.php

--- tcgp/index.php
+++ tcgp/index.php
@@ -187,12 +187,11 @@ html {
     -moz-background-size: cover;
     -o-background-size: cover;
     background-size: cover;
-    width: 100%;
+    height: 100%;
     overflow: hidden;
 }
 
 body{
-	background: none;
 		margin: 0;
 		padding: 0;
 		min-height: 100%;
