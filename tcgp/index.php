87f3f205e70f8a17be492b06941bad0e4e51f0cc
Arrow Sparrow <dev@aroceu.cloud>
2025-02-06 22:50:19 -0500

    Added TCGP

---
+++ tcgp/index.php
@@ -0,0 +1,343 @@
+<html lang="en">
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>MASTERBALL / TCGP</title>
+<meta name="viewport" content="initial-scale=1.0">
+<link rel="shortcut icon" type="image/x-icon" href="../../mball.ico" />
+<link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+
+<style type="text/css">
+
+	*{
+		margin: 0;
+		padding: 0;
+	}
+
+	body{
+background: url('bg.jpg') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+		margin: 0;
+		padding: 0;
+		font: normal 100%/160% 'Share Tech Mono', sans-serif;
+	}
+
+	#cover{
+		background: url('binder.png') no-repeat fixed center;
+		width: 100vw;
+	}
+
+	#container{
+		width: 69.5vw;
+		margin: 0 auto 0em;
+	}
+
+	#content{
+		padding-top: 2em;
+		margin-top: 40em;
+		width: auto;
+		background-color: #555;
+background-image: url('sandpaper.png');
+box-shadow: 0 -0.3em 0.35em #111;
+	}
+
+	h1{
+		font: normal 2em 'Silkscreen', monospace;
+		text-align: center;
+		color: #E8B625;
+		text-shadow: 0.08em 0.08em 0 #344B86;
+		padding-top: 1.5em;
+	}
+
+	#blurb{
+		background: #fafafa;
+		padding: 10px;
+		margin: 2em auto;
+		width: 25em;
+		border-radius: 20px;
+		  border: 1px solid #417067;
+
+	}
+
+	#card{
+		  border: #7bbccb 2px solid;
+		  border-radius: 15px;
+		  padding: 10px;
+
+	}
+
+	.binder {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2em;
+  width: auto;
+  margin: 0 auto;
+  border: 4px double #999;
+  border-radius: 30px;
+  width: 60vw;
+  background-color: #fff;
+background-image: url('lightpaper.png');
+}
+
+#scroll{
+	color: #edfcfb;
+	text-align: center;
+	font: normal 1.2em 'Silkscreen';
+	background: #344B86;
+	width: max-content;
+	margin: 0 auto;
+	opacity: 0.9;
+	box-shadow: 0.2em 0.2em 0 #E8B625;
+}
+
+b{
+	color: #417067;
+}
+
+i{
+	color: #7bbccb;
+}
+
+.card {
+  flex: 25%;
+  max-width: 24%;
+  text-align: center;
+  margin: 0.2em auto;
+  border: 1px dashed #999;
+  padding: 2% 0 2%;
+  	opacity: 0.7;
+background-color: #fff;
+background-image: url('cardboard.png');
+}
+
+.card img{
+	height: 250px;
+	width: 180px;
+  vertical-align: middle;
+  border: 2px solid #344B86;
+  border-radius: 8px;
+}
+
+.card:hover{
+	opacity: 1;
+}
+
+#footer{
+	font: normal 1.3em 'Silkscreen', monospace;
+	text-align: center;
+	padding-top: 2em;
+	padding-bottom: 2em;
+}
+
+.footer{
+	background: #fafafa;
+	width: fit-content;
+	text-align: center;
+	margin: 0 auto;
+	color: #344B86;
+}
+
+.footer a:link, .footer a:visited{
+	color: #344B86;
+	text-decoration: none;
+}
+
+.footer a:hover, .footer a:focus{
+	color: #7bbccb;
+}
+
+</style>
+
+</head>
+
+<body>
+
+	<div id="cover">
+	
+
+	<div id="container">
+
+				<h1>Trading Card Game Pocket</h1>
+
+		<div id="blurb">
+
+			<div id="card">
+			<p><b>Display Name:</b> a</p>
+			<p><b>Level:</b> 22</p>
+			<br />
+			<p align="center"><b><i>"I like Pok&eacute;mon from the Hoenn region!"</i></b></p>
+<br />
+			<p><b>Cards Owned:</b> 1,124</p>
+			<p><b>Friend Code:</b> 9874-4616-4192-4458</p>
+
+		</div>
+		</div>
+
+		<div id="scroll">Scroll down to view my binder &darr;</div>
+
+		<div id="content">
+
+		<div class="binder">
+
+			<div class="card">
+				<img src="cards/charizard-ex-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/pikachu-ex-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/celebi-ex-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/gengar-ex-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/marowak-ex-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/koga-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/expeditioner-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/bulbasaur-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/charmander-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/lapras-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/electrode-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/alakazam-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/cubone-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/nidoqueen-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/nidoking-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/golbat-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/weezing-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/dragonite-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/pidgeot-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/porygon-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/exeggutor-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/salandit-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/vaporeon-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/dedenne-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/marshadow-illus.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/venusaur-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/arcanine-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/articuno-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/gengar-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/mewtwo-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/machamp-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/marowak-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/celebi-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/mew-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/pidgeot-ex.png" alt="">
+			</div>
+
+			<div class="card">
+				<img src="cards/pachirisu-ex.png" alt="">
+			</div>
+
+		</div>
+
+		<div id="footer">
+	<p class="footer"><a href="https://icirr.us/home">Home</a> :: <a href="https://icirr.us/trainer">Trainer</a> :: <a href="https://icirr.us/gym">Gym</a> :: <a href="https://icirr.us/map">Map</a></p>
+
+</div>
+
+	</div>
+
+</div>
+
+
+</body>
+
+
+
